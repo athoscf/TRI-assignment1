667b76bac7fb362ec417c5f00f3f2c76b5b7892f
Athos Freitas <athos.coghi@gmail.com>
2025-03-24 17:39:30 +0000

    updated wall following policy and better sensor position

--- src/ObstacleAvoider.cpp
+++ src/ObstacleAvoider.cpp
@@ -1,8 +1,10 @@
 #include "firstrobot_webots/ObstacleAvoider.hpp"
 
 #define MAX_RANGE 0.15
-#define OPTIMAL_DISTANCE 0.12  // Target distance from the wall (in meters)
-#define DISTANCE_TOLERANCE 0.02  // Acceptable range around optimal distance
+#define MIN_RANGE 0.06
+#define OPTIMAL_DISTANCE 0.12 // Target distance from the wall (in meters)
+#define DISTANCE_TOLERANCE 0.01  // Acceptable range around optimal distance
+#define REVERSE_VEL -0.05
 
 ObstacleAvoider::ObstacleAvoider() : Node("obstacle_avoider") {
 
@@ -43,27 +45,31 @@ void ObstacleAvoider::rightSensorCallback(
 
   // Constant forward velocity
   command_message->linear.x = 0.1;  
-  command_message->angular.z = 1.0;  // Default to no rotation
+  command_message->angular.z = 0.0;  // Default to no rotation
   
 
-  // Wall following logic for right side
-  if (right_sensor_value > OPTIMAL_DISTANCE + DISTANCE_TOLERANCE) {
-    // Too far from wall, turn right
-    command_message->angular.z = -1.0;
+  if (right_sensor_value < MAX_RANGE * 0.95){
+    if (right_sensor_value < MIN_RANGE * 1.1){
+      command_message->linear.x = REVERSE_VEL;
+    } 
+    else{
+      // Wall following logic for right side
+      if (right_sensor_value > OPTIMAL_DISTANCE + DISTANCE_TOLERANCE) {
+        // Too far from wall, turn right
+        command_message->angular.z = -1.0;
+      } 
+      else if (right_sensor_value < OPTIMAL_DISTANCE - DISTANCE_TOLERANCE) {
+        // Too close to wall, turn left
+        command_message->angular.z = 1.0;
+      }
+    } 
   } 
-  else if (right_sensor_value < OPTIMAL_DISTANCE - DISTANCE_TOLERANCE) {
-    // Too close to wall, turn left
-    command_message->angular.z = 1.0;
+  else { //right sensor does not detect any object
+    // keep turning right in place
+    command_message->linear.x = 0.0;  
+    command_message->angular.z = -1.0; 
   }
-  // If within tolerance (OPTIMAL_DISTANCE ± DISTANCE_TOLERANCE), 
-  // angular.z remains 0.0, robot goes straight
-/*
-  // Emergency stop if left sensor detects very close obstacle
-  if (left_sensor_value < 0.5 * MAX_RANGE) {
-    command_message->linear.x = 0.0;
-    command_message->angular.z = -2.0;  // Turn away from left obstacle
-  }
-*/
+
   publisher_->publish(std::move(command_message));
 }
 
